8ea99df6aaca754db8c611ea48a5033430c19fe0
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-01-25 23:28:00 +0100

    feat: add auth guard in users and games

--- src/routes/games.ts
+++ src/routes/games.ts
@@ -1,8 +1,9 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
+import { authJWTOnRequest } from '../plugins/authenticate'
 
 export async function gameRoutes(fastify: FastifyInstance) {
-  fastify.get('/games/count', async () => {
+  fastify.get('/games/count', authJWTOnRequest, async () => {
     const count = await prisma.game.count()
     return { count }
   })
