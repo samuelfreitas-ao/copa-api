cae205070acbaf2bf5ed77ee0bb0c4aed5241f2f
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-01-26 11:36:35 +0100

    feat: game

--- src/routes/games.ts
+++ src/routes/games.ts
@@ -1,10 +1,38 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { prisma } from '../lib/prisma'
 import { authJWTOnRequest } from '../plugins/authenticate'
 
 export async function gameRoutes(fastify: FastifyInstance) {
-  fastify.get('/games/count', authJWTOnRequest, async () => {
-    const count = await prisma.game.count()
-    return { count }
+  fastify.get('/polls/:pollId/games', authJWTOnRequest, async (request) => {
+    const getPollParams = z.object({
+      pollId: z.string(),
+    })
+
+    const { pollId } = getPollParams.parse(request.params)
+    const games = await prisma.game.findMany({
+      orderBy: {
+        date: 'desc',
+      },
+      include: {
+        guesses: {
+          where: {
+            participant: {
+              userId: request.user.sub,
+              pollId,
+            },
+          },
+        },
+      },
+    })
+    return {
+      games: games.map((game) => {
+        return {
+          ...game,
+          guess: game.guesses.length > 0 ? game.guesses[0] : null,
+          guesses: undefined,
+        }
+      }),
+    }
   })
 }
